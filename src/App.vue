30e6abb3ec17220b1021281fb04a69bb6af6111b
Ram Akasaka <142519662+ihell@users.noreply.github.com>
2024-11-03 16:13:13 +0700

    init_

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,56 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-</script>
-
 <template>
-  <RouterView />
+  <div id="app" class="p-6 bg-gray-50 min-h-screen">
+    <inventory-form
+      :item="selectedItem"
+      :isEdit="isEdit"
+      @submit-item="saveItem"
+    />
+    <inventory-list
+      :items="items"
+      @edit-item="editItem"
+      @delete-item="deleteItem"
+    />
+  </div>
 </template>
+
+<script>
+import InventoryForm from './components/icons/InventoryForm.vue';
+import InventoryList from './components/icons/InventoryList.vue';
+
+export default {
+  components: {
+    InventoryForm,
+    InventoryList,
+  },
+  data() {
+    return {
+      items: [],
+      selectedItem: { name: '', stock: 0, price: 0 },
+      isEdit: false,
+    };
+  },
+  methods: {
+    saveItem(item) {
+      if (this.isEdit) {
+        const index = this.items.findIndex((i) => i.id === item.id);
+        if (index !== -1) this.items.splice(index, 1, item);
+      } else {
+        item.id = Date.now();
+        this.items.push(item);
+      }
+      this.resetForm();
+    },
+    editItem(item) {
+      this.selectedItem = { ...item };
+      this.isEdit = true;
+    },
+    deleteItem(id) {
+      this.items = this.items.filter((item) => item.id !== id);
+    },
+    resetForm() {
+      this.selectedItem = { name: '', stock: 0, price: 0 };
+      this.isEdit = false;
+    },
+  },
+};
+</script>
---
+++ src/components/icons/InventoryForm.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="p-6 bg-white shadow-md rounded-lg mb-4">
+      <h2 class="text-xl font-semibold mb-4">{{ isEdit ? 'Edit Item' : 'Tambah Item Baru' }}</h2>
+      <form @submit.prevent="submitForm" class="space-y-4">
+        <input v-model="item.name" placeholder="Nama Produk" required class="w-full p-2 border border-gray-300 rounded-lg" />
+        <input type="number" v-model="item.stock" placeholder="Stok" required class="w-full p-2 border border-gray-300 rounded-lg" />
+        <input type="number" v-model="item.price" placeholder="Harga" required class="w-full p-2 border border-gray-300 rounded-lg" />
+        <button type="submit" class="bg-green-500 text-white px-4 py-2 rounded-lg">{{ isEdit ? 'Simpan' : 'Tambah' }}</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['item', 'isEdit'],
+    methods: {
+      submitForm() {
+        this.$emit('submit-item', this.item);
+      }
+    }
+  };
+  </script>
+  
